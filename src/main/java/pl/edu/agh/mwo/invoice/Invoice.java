73ea1a444bc51d982374157b4e9aa6ca17d14f9b
Grzegorz Koziel <kniadziu@o2.pl>
2020-01-19 16:02:56 +0100

    Testy HashMap

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,45 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Map<Product, Integer> products = new HashMap<>();
+	
+	
+	//private Collection<Product> products;
 
-	public void addProduct(Product product) {
-		// TODO: implement
+	public void addProduct(Product product) { //konstruktor z nazwa zwraz=ca 1 produkt
+		//dodac ify
+		this.products.put(product,1);
 	}
 
-	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+	public void addProduct(Product product, Integer quantity) { //konstruktor zwraza produkt i jego ilsc
+		this.products.put(product,quantity);
 	}
 
 	public BigDecimal getNetPrice() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
+		for  (Product product: this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum= sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return BigDecimal.ZERO;
+		return this.getGrossPrice().subtract(this.getNetPrice());
 	}
 
 	public BigDecimal getGrossPrice() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
+		for  (Product product: this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum= sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+		}
+		return sum;
 	}
 }
