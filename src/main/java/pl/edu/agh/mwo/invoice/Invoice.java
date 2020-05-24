e690f63005090fef06b148eb8a4fbc43705a7822
Grzegorz Koziel <kniadziu@o2.pl>
2020-05-24 18:41:18 +0200

    Style correction

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,114 +3,108 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 
-	private Map<Product, Integer> products = new HashMap<>();
+    private Map<Product, Integer> products = new HashMap<>();
 
-	private static Integer nextNumber=0;
-	private Integer number=++nextNumber; //nr faktury
+    private static Integer nextNumber = 0;
+    private Integer number = ++nextNumber;
+    public Integer getNumber() {
+        return number;
+    }
 
-	public Integer getNumber() {
-		return number;
-	}
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 
-	public void setNumber(Integer number) {
-		this.number = number;
-	}
+    public void addProduct(Product product) { //konstruktor z nazwa zwraz=ca 1 produkt
+        this.products.put(product, 1);
+    }
 
-	//private Collection<Product> products;
+    public void addProduct(Product product, Integer quantity) { //konstruktor zwraza produkt i jego ilsc
+//      walidujemy czy parametry sa poprawne dla quantity =0
+        if (quantity <= 0) { //sprawdzenie quantity jest zero lub mniejsze
+            throw new IllegalArgumentException("Ilośc nie może być zero.");
+        }
+//      jesli dodajesz ten sam produkt do fv to zwieksz liczbe pozycji
+        if (products.containsKey(product)) {
+            products.put(product, products.get(product) + quantity);
+        } else {
+            products.put(product, quantity);
+        }
+    }
 
-	public void addProduct(Product product) { //konstruktor z nazwa zwraz=ca 1 produkt
-		//dodac ify
-		this.products.put(product,1);
-	}
+    public BigDecimal getNetPrice() {
+        BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+        }
+        return sum;
+    }
 
-	public void addProduct(Product product, Integer quantity) { //konstruktor zwraza produkt i jego ilsc
-		//walidujemy czy parametry sa poprawne dla quantity =0
-		if (quantity <=0){ //sprawdzenie quantity jest zero lub mniejsze
-			throw new IllegalArgumentException("Ilośc nie może być zero.");
-		}
-		//jesli dodajesz ten sam produkt do fv to zwieksz liczbe pozycji
-		if (products.containsKey(product)) {
-			products.put(product, products.get(product) + quantity);
-		} else {
-			products.put(product, quantity);
-		}
-	}
+    public BigDecimal getTax() {
+        return this.getGrossPrice().subtract(this.getNetPrice());
+    }
 
-	public BigDecimal getNetPrice() {
-		BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
-		for  (Product product: this.products.keySet()) {
-			Integer quantity = this.products.get(product);
-			sum= sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
-		}
-		return sum;
-	}
+    public BigDecimal getGrossPrice() {
+        BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            sum = sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+        }
+        return sum;
+    }
 
-	public BigDecimal getTax() {
-		return this.getGrossPrice().subtract(this.getNetPrice());
-	}
-
-	public BigDecimal getGrossPrice() {
-		BigDecimal sum = BigDecimal.ZERO; //przypisujemy do zmiennej zero
-		for  (Product product: this.products.keySet()) {
-			Integer quantity = this.products.get(product);
-			sum= sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
-		}
-		return sum;
-	}
-
-	//DRKOWANIE FAKTURY
-	public int print(){
-		int lp=0;
-		System.out.println("Faktura  nr: " + getNumber());
-		System.out.println("-------------------------------------------------");
-		System.out.printf("%2s | %-20s | %5s | %10s |", "Lp.","Nazwa produktu", "Ilość" , "Cena jedn.");
-		System.out.println("\n-------------------------------------------------");
-		for (Product product : products.keySet()){
-			lp++;
-			System.out.printf(" %2s | %-20s | %5s | %10s |\n",lp, product. getName(),  products.get(product) , product.getPriceWithTax());
-		}
-		System.out.println("-------------------------------------------------\n");
- 		return lp;
-	}
+//  DRKOWANIE FAKTURY
+    public int print() {
+        int lp = 0;
+        System.out.println("Faktura  nr: " + getNumber());
+        System.out.println("-------------------------------------------------");
+        System.out.printf("%2s | %-20s | %5s | %10s |", "Lp.", "Nazwa produktu", "Ilość", "Cena jedn.");
+        System.out.println("\n-------------------------------------------------");
+        for (Product product : products.keySet()) {
+            lp++;
+            System.out.printf(" %2s | %-20s | %5s | %10s |\n", lp, product.getName(), products.get(product), product.getPriceWithTax());
+        }
+        System.out.println("-------------------------------------------------\n");
+        return lp;
+    }
 
 
-	public int getQuantityProducts(){
-		int quantity=0;
-		for (Product product : products.keySet()){
-			quantity++;
-	}
-		return quantity;
-	}
+    public int getQuantityProducts() {
+        int quantity = 0;
+        for (Product product : products.keySet()) {
+            quantity++;
+        }
+        return quantity;
+    }
 
 
-	public BigDecimal getGrossTotal() {
-		BigDecimal totalGross = BigDecimal.ZERO;
-		for (Product product : products.keySet()) {
-			BigDecimal quantity = new BigDecimal(products.get(product));
-			totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
-		}
-		return totalGross;
-	}
+    public BigDecimal getGrossTotal() {
+        BigDecimal totalGross = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
+        }
+        return totalGross;
+    }
 
 
+    public BigDecimal getNetTotal() {
+        BigDecimal totalNet = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalNet = totalNet.add(product.getPrice().multiply(quantity));
+        }
+        return totalNet;
+    }
 
-	public BigDecimal getNetTotal() {
-		BigDecimal totalNet = BigDecimal.ZERO;
-		for (Product product : products.keySet()) {
-			BigDecimal quantity = new BigDecimal(products.get(product));
-			totalNet = totalNet.add(product.getPrice().multiply(quantity));
-		}
-		return totalNet;
-	}
-	public BigDecimal getTaxTotal() {
-		return getGrossTotal().subtract(getNetTotal());
-	}
-
+    public BigDecimal getTaxTotal() {
+        return getGrossTotal().subtract(getNetTotal());
+    }
 
 
 }
