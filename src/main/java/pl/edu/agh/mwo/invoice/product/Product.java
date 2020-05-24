982c71352d68cb3d8affb9cb4e3f23f8e7a97e8c
Grzegorz Koziel <kniadziu@o2.pl>
2020-05-24 19:06:02 +0200

    Style correction no. 6

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -9,8 +9,8 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-        //		walidujemy czy parametry sa poprawne wg domeny, gdzie sa
-        // 		implementowane, czy moga byc null
+        // walidujemy czy parametry sa poprawne wg domeny, gdzie sa
+        // implementowane, czy moga byc null
         if (name == null || name.isEmpty()) { //sprawdzenie czy nazwa jest pusta lub null,
             // kolejnosc jest wazna, bo po 1 sprawdzeniu juz nie sprawdza dalej
             throw new IllegalArgumentException("Nazwa produktu nie może być null.");
